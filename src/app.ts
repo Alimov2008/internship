75ad60a16503286213972b8886540688fd9b6fa2
Ali <alimovmuhammadali01222008@gmail.com>
2025-12-24 17:50:39 +0500

    implemented centralized error handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import healthRoutes from "./routes/health.routes";
 import notesRoutes from "./routes/notes.routes";
+import { errorHandler } from "./middlewares/error.middleware";
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use(healthRoutes);
 app.use(notesRoutes);
+app.use(errorHandler);
 
 export default app;
