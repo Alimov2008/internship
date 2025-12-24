7778b5eefe70620d278d2b01a2a45fa0eca8f99b
Ali <alimovmuhammadali01222008@gmail.com>
2025-12-24 16:34:55 +0500

    initial note services

--- src/services/notes.service.ts
+++ src/services/notes.service.ts
@@ -1,3 +1,14 @@
 import { notes } from "../data/notes.store";
 import { Note } from "../models/note.model";
 import { randomUUID } from "crypto";
+
+export const NotesService = {
+  getAll(): Note[] {
+    return notes;
+  },
+
+  getById(id: string): Note | undefined {
+    return notes.find(note => note.id === id);
+  },
+
+};
