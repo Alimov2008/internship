1731eeb25c6a6efe265c979094739ebe7d98982b
Ali <alimovmuhammadali01222008@gmail.com>
2025-12-24 17:48:48 +0500

    applied validation inrouts

--- src/routes/notes.routes.ts
+++ src/routes/notes.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { NotesController } from "../controllers/notes.controller";
+import { validate } from "../middlewares/validation.middleware";
+import { createNoteSchema, updateNoteSchema, noteIdParamSchema } from "../validators/notes.schema";
 
 const router = Router();
 
 router.get("/notes", NotesController.getAll);
-router.get("/notes/:id", NotesController.getById);
-router.post("/notes", NotesController.create);
-router.put("/notes/:id", NotesController.update);
-router.delete("/notes/:id", NotesController.delete);
+router.get("/notes/:id", validate(noteIdParamSchema, "params"), NotesController.getById);
+router.post("/notes", validate(createNoteSchema), NotesController.create);
+router.put("/notes/:id", validate(noteIdParamSchema, "params"), validate(updateNoteSchema), NotesController.update);
+router.delete("/notes/:id", validate(noteIdParamSchema, "params"), NotesController.delete);
 
 export default router;
